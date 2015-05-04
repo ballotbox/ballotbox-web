63d1a3f0b60011edde54066d6997191909060ed8
Richard Young <richiejoeyoung@gmail.com>
2015-05-04 00:11:10 -0400

    adds api/v1/elections#create

--- app/controllers/api/v1/elections_controller.rb
+++ app/controllers/api/v1/elections_controller.rb
@@ -9,11 +9,28 @@ module Api::V1
     def show
       e = Election.find(params[:id])
       render json: {
-          election: { title: e.title,
-                      body: e.body,
-                      choices: e.choices.map do |c|
+         election: {  title:    e.title,
+                      body:     e.body,
+                      choices:  e.choices.map do |c|
                         { body: c.body, count: c.votes.count }
                       end }}
     end
+
+    def create
+      election = Election.create! do |e|
+        e.title      = params[:election][:title]
+        e.body       = params[:election][:body]
+        e.public     = params[:election][:public]
+        e.creator_id = current_resource_owner.id
+      end
+      params[:election][:choices].each do |c|
+        election.choices.create! do |choice|
+          choice.body        = c[:body]
+          choice.election_id = election.id 
+        end
+      end
+      redirect_to api_v1_election_path(election), status: 201
+    end
+
   end
 end
